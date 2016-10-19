0083407329f56359585601828a3987f2330aca83
Joseph0666 <win.joseph0666@gmail.com>
2016-10-19 22:14:01 +0530

    Add files via upload

---
+++ vowels.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	char a,e,i,o,u,word,A,E,I,O,U;
+	printf("enter the word");
+	scanf("%c",&word);
+	switch(word)
+	{
+		case'a':
+			case'A':
+			printf("its is vowel");
+			break;
+			case'e':
+				case'E':
+					printf("its is vowel");
+					break;
+					case'i':
+						case'I':
+							printf("its is vowel");
+							break;
+							case'o':
+								case'O':
+									printf("its is vowel");
+									break;
+									case'u':
+									case'U':	
+									printf("its is vowel");
+									break;
+									default :
+	printf("its is consonants");
+}
+
+
+	}
+	
