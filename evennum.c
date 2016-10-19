2ece6ec33c83b64eb8bbbff7fad727a0faeac9c2
Joseph0666 <win.joseph0666@gmail.com>
2016-10-19 21:54:27 +0530

    Add files via upload

---
+++ evennum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int num;
+	printf("enter the num");
+	scanf("%d",&num);
+	if(num%2==0)
+	{
+		printf("the num is even");
+	}
+	else
+	{
+		printf("the num is odd");
+	}
+//	getch();
+}
