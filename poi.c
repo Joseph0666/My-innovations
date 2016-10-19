a714ce6250135b423e8a032ff29681011d8ac851
Joseph0666 <win.joseph0666@gmail.com>
2016-10-19 21:46:12 +0530

    Add files via upload

---
+++ poi.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int num;
+	printf("enter the number");
+	scanf("%d",&num);
+	if(num>0)
+	{
+		printf("the num is poisitive");
+	}
+	else
+	{
+		printf("the num  is negative");
+	}
+//	getch();
+}
