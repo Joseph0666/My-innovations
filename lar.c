5ff28ac7eb83ee5fbf54e85908a06a6a2b0054b0
Joseph0666 <win.joseph0666@gmail.com>
2016-10-20 21:06:11 +0530

    Add files via upload

---
+++ lar.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b,c;
+printf("enter the a");
+scanf("%d",&a);
+printf("enter the b");
+scanf("%d",&b);
+printf("enter the c");
+scanf("%d",&c);
+if(a>b)
+{
+	printf("a is big");
+}
+else if(b>c)
+{
+	printf("b is big");
+}
+else
+{
+	printf("c is big");
+}
+}
+
+
