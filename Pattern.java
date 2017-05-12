fbf5e68d2222d97a6fdb2afe2aa55f169e0981ec
Joseph0666 <win.joseph0666@gmail.com>
2017-05-13 00:21:46 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,22 @@
+package pro;
+import java.util.Scanner;
+public class Pattern {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int n1=n*2,i,j;
+		for(i=0;i<n;i++)
+		{
+			for(j=i;j<n;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.print(" ");
+			for(j=i;j<n;j++){
+			System.out.print("*");}
+			System.out.println("\n");
+		}
+	}
+
+}
