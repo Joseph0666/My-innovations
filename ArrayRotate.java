29ba13005e7a145c214f040f205699091be1670c
Joseph0666 <win.joseph0666@gmail.com>
2017-05-27 00:28:34 +0530

    Add files via upload

--- ArrayRotate.java
+++ ArrayRotate.java
@@ -6,7 +6,7 @@ public class ArrayRotate {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
 		int[] a=new int[n];
-		System.out.println("Enter d");
+		System.out.println("Enter d value less than half of n");
 		int d=s.nextInt();
 		int i,j;
 		int[] b=new int[n];
@@ -14,11 +14,34 @@ public class ArrayRotate {
 		{
 			a[i]=s.nextInt();
 		}
+		if(n%2==0){
 		for(i=d;i<n;i++)
 		{
 			b[i-d]=a[i];
+		}
+		for(i=0;i<d;i++)
+		{
+			b[d+i]=a[i];
+		}		
+		for(i=0;i<n;i++)
+		{
 			System.out.println(b[i]);
 		}
+		}
+		else
+		{
+			for(i=d;i<n;i++)
+			{
+				b[i-d]=a[i];
+			}
+			for(i=0;i<d;i++)
+			{
+				b[d+i+1]=a[i];
+			}		
+			for(i=0;i<n;i++)
+			{
+				System.out.println(b[i]);
+			}
+		}
 	}
-
 }
