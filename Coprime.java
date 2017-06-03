c0cdaef9828e6322281b77782f014b7c2f3b9bb7
Joseph0666 <win.joseph0666@gmail.com>
2017-06-04 00:01:43 +0530

    Add files via upload

---
+++ Coprime.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Coprime {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int input1=s.nextInt();
+		int input2=s.nextInt();
+		int i,count=0,count1=0;
+		for(i=1;i<input1;i++)
+		{
+			if(input1%i==0)
+			{
+				count++;
+			}
+		}
+		for(i=1;i<input2;i++)
+		{
+			if(input2%i==0)
+			{
+				count1++;
+			}
+		}
+		if(count==1&&count1==1)
+		{
+			System.out.println("Coprime num");
+		}
+		else
+		{
+			System.out.println("Not coprime num");
+		}
+	}
+
+}
