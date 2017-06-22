c18d4c6ddb84ca05abba17edaea71ec2f7be36f8
Joseph0666 <win.joseph0666@gmail.com>
2017-06-22 16:35:10 +0530

    Add files via upload

---
+++ ReverseWords.java
@@ -0,0 +1,24 @@
+package LogicPrograms;
+
+public class ReverseWords {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="Hello World";
+		char[] ch=str.toCharArray();
+		int i;
+		String s1="";
+		for(i=str.length()-1;i>=0;i--){
+			s1=s1+ch[i];
+		}
+		System.out.println(s1);
+		String s4="";
+		String[] s3=s1.split(" ");
+		for(String w:s3){
+			StringBuffer s2=new StringBuffer(w);
+			s2.reverse().toString();
+			s4=s4+s2+" ";
+			}
+		System.out.println(s4);
+	}
+
+}
