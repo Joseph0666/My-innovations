0c6c4589e303f620568f12dea1499bffa5c41868
Joseph0666 <win.joseph0666@gmail.com>
2017-07-14 16:03:14 +0530

    Update ReversedWords.java

--- ReversedWords.java
+++ ReversedWords.java
@@ -1,5 +1,6 @@
 package pro;
 import java.util.Scanner;
+import java.util.ArrayList;
 public class ReversedWords {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -7,12 +8,24 @@ public class ReversedWords {
 		System.out.println("Enter String");
 		String str=s.nextLine();
 		String[] str1=str.split(" ");
+		ArrayList<String> list = new ArrayList<String>();
 		for(String str2:str1)
 		{
-			StringBuffer rev=new StringBuffer(str2);
-			rev.reverse();
-			System.out.print(rev+" ");
+			list.add(str2);
+		}String s1="";
+		for(int i=0;i<list.size();i++){
+			if(i%2==0){
+				String s2="";
+				s2=s2+list.get(i);
+				StringBuffer s3 = new StringBuffer(s2);
+				s3.reverse().toString();
+				s1=s1+s3+" ";
+			}
+			else{
+				s1=s1+list.get(i)+" ";
+			}
 		}
+		System.out.println(s1);
 	}
 
 }
