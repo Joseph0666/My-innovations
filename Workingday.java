f0a541fd045d09b6a4ef801cd9edc897f6260d05
Joseph0666 <win.joseph0666@gmail.com>
2017-07-14 15:44:08 +0530

    Update Workingday.java

--- Workingday.java
+++ Workingday.java
@@ -6,11 +6,11 @@ public class Workingday {
 		Scanner s=new Scanner(System.in);
 		System.out.println("Enter the day 1st char to be in uppercase and remaning char in lowercase");
 		String str=s.nextLine();
-		if(str.equalsIgnoreCase("Sunday")||str.equalsIgnoreCase("Saturday"))
+		if(str.equalsIgnoreCase("Sunday"))
 		{
 			System.out.println("False");
 		}
-		else if(str.equalsIgnoreCase("Monday")||str.equalsIgnoreCase("Tuesday")||str.equalsIgnoreCase("Wednesday")||str.equalsIgnoreCase("Thrusday")||str.equalsIgnoreCase("Friday"))
+		else if(str.equalsIgnoreCase("Saturday")||str.equalsIgnoreCase("Monday")||str.equalsIgnoreCase("Tuesday")||str.equalsIgnoreCase("Wednesday")||str.equalsIgnoreCase("Thrusday")||str.equalsIgnoreCase("Friday"))
 		{
 			System.out.println("True");
 		}
